e294ef9b6a59beae9711fee15be670ed2d696d4f
RoelWeening <66599005+RoelWeening@users.noreply.github.com>
2024-09-27 17:47:13 +0200

    Update unbound-run.sh

--- files/unbound-run.sh
+++ files/unbound-run.sh
@@ -1,3 +1,2 @@
-#!/usr/bin/execlineb -P
-with-contenv
-/usr/sbin/unbound -d -c /etc/unbound/unbound.conf.d/pi-hole.conf
+#!/usr/bin/with-contenv bash
+exec /usr/sbin/unbound -d -c /etc/unbound/unbound.conf
